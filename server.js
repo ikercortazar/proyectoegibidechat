ad22638a304d234c4f35693898e20f7603a92f53
ikercortazar <ikermanjava@gmail.com>
2017-01-25 20:18:00 +0100

    Tercero

--- server.js
+++ server.js
@@ -13,5 +13,7 @@ app.get('/*', function(req, res) {
     var jsonResponse = [];
     jsonResponse.push({ "text": "Hi. " + (Math.random() * 5 + 1).toFixed(0) + " is a lucky number..." });
     res.send(jsonResponse);
+    jsonResponse.push({ "text": "Hi. " + (Math.random() * 5 + 1).toFixed(0) + " is a lucky number..." });
+    res.send(jsonResponse);
 });
 
