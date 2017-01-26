d4d53499c6f9f8994dbbb4729c213425d337a8d6
ikercortazar <ikermanjava@gmail.com>
2017-01-26 20:09:52 +0100

    Bueno2

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ app.listen(PORT, function() {
     console.log('Chatfuel Bot-Server listening on port '+PORT+'...');
 });
 
-app.get('/numero/:nume', function(req, res) {
+app.get("/numero/:nume", function (req, res) {
     var jsonResponse = [];
-    jsonResponse.push({ "texto": "Hi. " + (Math.random() * 5 + 1).toFixed(0) + " is a lucky number...  ♥"+req.params.nume});
+    jsonResponse.push({ "texto": "Hi. " + (Math.random() * 5 + 1).toFixed(0) + " is a lucky number...  ♥"+ req.params.nume});
     res.send(jsonResponse);
     jsonResponse.push({ "text": "Hi. " + (Math.random() * 5 + 1).toFixed(0) + " is a lucky number..." });
     res.send(jsonResponse);
